f7f4cee154763b1f46b9bd99d79e8a9792398679
megrivh <mpah97@hotmail.com>
2025-03-02 15:42:28 -0500

    Implemented random fact generator based on TableView category selection

--- FunFactGenerator/ViewController.swift
+++ FunFactGenerator/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
     var categories = ["Animals", "Food", "History", "Science", "Space", "Weird Laws", "Technology", "Movies", "Music", "Random"]
+    var generatorLogic = GeneratorLogic()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories.count
@@ -20,6 +23,40 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         content.text = categories[indexPath.row]
         cell.contentConfiguration = content
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedCategory = categories[indexPath.row]
+        print("The selected category is: \(selectedCategory)")
+        let factText = generateFact(selectedCategory)
+        print("The fact is: \(factText)")
+    }
+        
+    func generateFact(_ category: String) -> String {
+        switch category {
+        case "Animals":
+            return generatorLogic.getAnimalFact()
+        case "Food":
+            return generatorLogic.getFoodFacts()
+        case "History":
+            return generatorLogic.getHistoryFacts()
+        case "Science":
+            return generatorLogic.getScienceFacts()
+        case "Space":
+            return generatorLogic.getSpaceFacts()
+        case "Weird Laws":
+            return generatorLogic.getWeirdLawsFacts()
+        case "Technology":
+            return generatorLogic.getTechnologyFacts()
+        case "Movies":
+            return generatorLogic.getMovieFacts()
+        case "Music":
+            return generatorLogic.getMusicFacts()
+        case "Random":
+            return generatorLogic.getRandomFact()
+        default :
+            return "I don't have a fact for that category."
+        }
     }
     
     override func viewDidLoad() {
