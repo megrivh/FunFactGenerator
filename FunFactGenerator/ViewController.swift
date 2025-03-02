8d42e7f9ff709543c101c74151d83100b89cb6fc
megrivh <mpah97@hotmail.com>
2025-03-02 12:55:12 -0500

    Set up TableView UI and added categories to populate cells

--- FunFactGenerator/ViewController.swift
+++ FunFactGenerator/ViewController.swift
@@ -7,8 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var categories = ["Animals", "Food", "History", "Science", "Space", "Weird Laws", "Technology", "Movies", "Music", "Random"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
+        var content = cell.defaultContentConfiguration( )
+        content.text = categories[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
