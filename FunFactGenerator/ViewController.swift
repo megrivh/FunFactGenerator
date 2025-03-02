c68c54366b070bf8ba987f0d2a9693e6f871dd78
megrivh <mpah97@hotmail.com>
2025-03-02 12:08:41 -0500

    Initial Commit

---
+++ FunFactGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunFactGenerator
+//
+//  Created by Megan Rivera Hammock on 3/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
